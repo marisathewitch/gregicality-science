24059505614cf561ad5fa04a4f76a5ba0ad086cb
brachy84 <brachy84@gmail.com>
2022-04-05 06:42:00 +0200

    rendering, lang & more registry

--- src/main/java/gregicality/science/client/render/GCYSTextures.java
+++ src/main/java/gregicality/science/client/render/GCYSTextures.java
@@ -2,8 +2,12 @@ package gregicality.science.client.render;
 
 import gregicality.science.GregicalityScience;
 import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static gregtech.client.renderer.texture.cube.OrientedOverlayRenderer.OverlayFace.*;
 
@@ -14,5 +18,14 @@ public class GCYSTextures {
     public static OrientedOverlayRenderer CRYSTALLIZATION_CRUCIBLE_OVERLAY = new OrientedOverlayRenderer("multiblock/crystallization_crucible", FRONT);
     public static OrientedOverlayRenderer ROASTER_OVERLAY = new OrientedOverlayRenderer("multiblock/roaster", FRONT);
     public static OrientedOverlayRenderer NANOSCALE_FABRICATOR_OVERLAY = new OrientedOverlayRenderer("multiblock/nanoscale_fabricator", FRONT);
+
+    public static TextureAtlasSprite PRESSURE_PIPE_SIDE;
+    public static TextureAtlasSprite PRESSURE_PIPE_OPEN;
+
+    @SideOnly(Side.CLIENT)
+    public static void register(TextureMap textureMap) {
+        PRESSURE_PIPE_SIDE = textureMap.registerSprite(new ResourceLocation(GregicalityScience.MODID, "blocks/pipe/pressure_pipe_side"));
+        PRESSURE_PIPE_OPEN = textureMap.registerSprite(new ResourceLocation(GregicalityScience.MODID, "blocks/pipe/pressure_pipe_open"));
+    }
 
 }
